cd23d291f16b1e0df0d32a8907f4e8059d242bc6
Miniberry <76786838+ljh8450@users.noreply.github.com>
2025-12-09 01:10:35 +0900

    Delete backend directory

--- backend/src/main/java/com/example/AnimAI/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.animai.repository;
-
-import com.example.animai.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
